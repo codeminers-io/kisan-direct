982ac4f33455470f782c6f0bb34b8e92f755b674
Jibin Punchepady <jibin.pb@gmail.com>
2020-07-11 18:38:18 +0800

    added sample images

--- public/urls.py
+++ public/urls.py
@@ -4,4 +4,7 @@ from .views import home
 
 urlpatterns = [
     path('', home.index, name='index'),
+    path('apple-touch-icon.png', home.apple_touch_icon_png, name='apple_touch_icon_png'),
+    path('favicon.png', home.favicon_png, name='favicon_png'),
+    path('favicon.ico', home.favicon_ico, name='favicon_ico'),
 ]
--- public/views/home.py
+++ public/views/home.py
@@ -1,7 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import os
 
 
 def index(request):
     context = {'latest_question_list': "latest_question_list"}
     return render(request, 'home/index.html', context)
+
+
+def apple_touch_icon_png(request):
+    valid_image = os.path.join(os.path.dirname(os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__)))), "static/img/logo-180x180.png")
+    with open(valid_image, "rb") as f:
+        return HttpResponse(f.read(), content_type="image/png")
+
+
+def favicon_png(request):
+    valid_image = os.path.join(os.path.dirname(os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__)))), "static/img/logo-32x32.png")
+    with open(valid_image, "rb") as f:
+        return HttpResponse(f.read(), content_type="image/png")
+
+
+def favicon_ico(request):
+    valid_image = os.path.join(os.path.dirname(os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__)))), "static/img/favicon.ico")
+    with open(valid_image, "rb") as f:
+        return HttpResponse(f.read(), content_type="image/x-icon")
