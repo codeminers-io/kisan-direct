cd266009f52c8f5abe4bbaa4ebf8606fef12f2c4
Punchepady, Jibin <Jibin.PunchepadyBroonan@Schroders.com>
2020-07-13 12:10:59 +0000

    trying hindi in language

--- public/urls.py
+++ public/urls.py
@@ -4,6 +4,7 @@ from .views import home
 
 urlpatterns = [
     path('', home.index, name='index'),
+    path('switch_to_language_link/<language>', home.switch_to_language_link, name='switch_to_language_link'),
     path('apple-touch-icon.png', home.apple_touch_icon_png, name='apple_touch_icon_png'),
     path('favicon.png', home.favicon_png, name='favicon_png'),
     path('favicon.ico', home.favicon_ico, name='favicon_ico'),
--- public/views/home.py
+++ public/views/home.py
@@ -4,9 +4,12 @@ import os
 
 
 def index(request):
+    #request.session['lang'] = 'hi'
     context = {'latest_question_list': "latest_question_list"}
     return render(request, 'home/index.html', context)
 
+def switch_to_language_link(request, language='en'):
+    request.session['lang'] = language
 
 def apple_touch_icon_png(request):
     valid_image = os.path.join(os.path.dirname(os.path.dirname(
