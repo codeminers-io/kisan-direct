39f3e38e8e97195eda0f94c0abda8d02d14a0fd9
Jibin Punchepady <jibin.pb@hotmail.com>
2020-07-14 06:40:15 +0000

    adding login screen

--- kisan_direct/urls.py
+++ kisan_direct/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('public.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
