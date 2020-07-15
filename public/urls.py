eb54a169b8028338124bfa24637eb139382e90af
Jibin Punchepady <jibin.pb@hotmail.com>
2020-07-15 06:15:33 +0000

    added style changes

--- public/urls.py
+++ public/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('apple-touch-icon.png', home.apple_touch_icon_png, name='apple_touch_icon_png'),
     path('favicon.png', home.favicon_png, name='favicon_png'),
     path('favicon.ico', home.favicon_ico, name='favicon_ico'),
+    path('public/farmers', home.index, name='index'),
 ]
